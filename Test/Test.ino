e713ed8c8ed385ac237822afa2e5490a5fbafd8d
Nicolas Montoya <nicolasmontoya@yahoo.com>
2020-09-12 13:26:32 -0400

    RGB leds respond to change in y axis

--- Test/Test.ino
+++ Test/Test.ino
@@ -2,8 +2,8 @@
 #include "Wire.h"
 #include <MPU6050_light.h>
 
-// How many leds in your strip?
-#define NUM_LEDS 5
+// How many leds in your strip? 28
+#define NUM_LEDS 28
 
 // For led chips like WS2812, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
@@ -20,15 +20,29 @@ MPU6050 mpu(Wire);
 unsigned long timer = 0;
 
 
-void setup() { 
-    // Uncomment/edit one of the following lines for your leds arrangement.
-    // ## Clockless types ##
-    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);  // GRB ordering is assumed
-    
+void setup() {
+  // Uncomment/edit one of the following lines for your leds arrangement.
+  // ## Clockless types ##
+  Serial.begin(9600);
+  Wire.begin();
+  mpu.begin();
+  Serial.println(F("Calculating gyro offset, do not move MPU6050"));
+  delay(1000);
+  mpu.calcGyroOffsets();
+  Serial.println("Done!\n");
+  
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);  // GRB ordering is assumed
+
 }
 
-void loop() { 
+void loop() {
   static uint8_t hue = 0;
-  FastLED.showColor(CHSV(hue++, 255, 255)); 
-  delay(10);
+  
+
+  mpu.update();
+  // y goes from -90 to 90
+  
+
+  FastLED.showColor(CHSV(map(mpu.getAngleY(), -90, 90, 0, 255), 255, 255));  
+  
 }
